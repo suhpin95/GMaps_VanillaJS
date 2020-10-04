f12d6aad07714a31e5c9dc325463f587dacbd52b
Suhas <suhpin95@gmail.com>
2020-10-04 14:12:42 +0530

    Added Info Table on click of Marker

--- scripts/index.js
+++ scripts/index.js
@@ -3,15 +3,24 @@ function initMap() {
     let position = {
         lat: 16.7692289,
         lng: 82.1291935,
+        nameOfCity : 'Kuyyeru'
     };
     let mapConfigObj = {
         center: position,
         zoom: 12,
     };
-
     let map = new google.maps.Map(mapDiv, mapConfigObj);
-    var marker = new google.maps.Marker({
+    const infowindow = new google.maps.InfoWindow({
+        content: position['nameOfCity'],
+      });
+    let marker = new google.maps.Marker({
         position,
         map,
     });
+    // Event Listener
+    marker.addListener("click", () => {
+        infowindow.open(map, marker);
+    });
 }
+
+
