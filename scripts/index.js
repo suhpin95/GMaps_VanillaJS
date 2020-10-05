3c2c05b6273c4883f9d837fc372bf9b8c47f6925
Suhas <suhpin95@gmail.com>
2020-10-05 10:50:35 +0530

    Adding the list using click event.Marker can be dragged

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,34 @@
 function initMap() {
     let mapDiv = document.getElementById("map");
-    let position = {
+    let initialPosition = {
         lat: 16.7692289,
         lng: 82.1291935,
         nameOfCity : 'Kuyyeru'
     };
     let mapConfigObj = {
-        center: position,
+        center: initialPosition,
         zoom: 12,
     };
     let map = new google.maps.Map(mapDiv, mapConfigObj);
     const infowindow = new google.maps.InfoWindow({
-        content: position['nameOfCity'],
+        content: initialPosition['nameOfCity'],
       });
     let marker = new google.maps.Marker({
-        position,
+        position: initialPosition,
         map,
+        draggable:true
     });
     // Event Listener
     marker.addListener("click", () => {
         infowindow.open(map, marker);
+        addList()
     });
-}
 
+    // Adding the places in the list
+    function addList () {
+        let toVisit = document.createElement("li");
+        toVisit.innerHTML = initialPosition['nameOfCity']
+        document.getElementById('cardList').append(toVisit)
+    }
+}
 
